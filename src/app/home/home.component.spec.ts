917a3cfc3bd49612420f03b750dd816d9c38b920
voodoo <voodoo@blackandyellowpages.com>
2019-10-28 14:23:27 -0700

    added home tests for carousel and category list components

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,14 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { CategoryListComponent } from '../category-list/category-list.component';
+
+@Component({
+  selector: 'app-question-carousel',
+  template: '<p>Mock question carousel component'
+})
+class MockQuestionCarouselComponent {};
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +16,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, MockQuestionCarouselComponent, CategoryListComponent ]
     })
     .compileComponents();
   }));
@@ -22,4 +30,14 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a Question Carousel component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-question-carousel')).not.toBe(null);
+  }));
+
+  it('should have a Categories List component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-category-list')).not.toBe(null);
+  }));
 });
--- src/app/question-show/question-show.component.spec.ts
+++ src/app/question-show/question-show.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuestionShowComponent } from './question-show.component';
+import { AnswerShowComponent } from '../answer-show/answer-show.component';
 
 describe('QuestionShowComponent', () => {
   let component: QuestionShowComponent;
