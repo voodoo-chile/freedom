8fc8cd5538e153bd013d9c5f07f3fd2fa3b976fb
voodoo <voodoo@blackandyellowpages.com>
2019-10-28 16:38:07 -0700

    added tests to answer-list component

--- src/app/answer-list/answer-list.component.spec.ts
+++ src/app/answer-list/answer-list.component.spec.ts
@@ -1,14 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AnswerListComponent } from './answer-list.component';
+import { AnswersService } from '../answers.service';
 
 describe('AnswerListComponent', () => {
   let component: AnswerListComponent;
   let fixture: ComponentFixture<AnswerListComponent>;
+  let expectedIds = ['aaa', 'bbb'];
+  let mockAnswers = [
+    {  
+      _id: 'aaa',
+      AnswerTitle: 'MockTitle',
+      AnswerAuthor: 'MockAuthor',
+      AnswerUrl: 'MockUrl',
+      AnswerBlurb: 'lorem impsum',
+      AnswerTags: 'mock tag '
+    }, 
+    {  
+      _id: 'bbb',
+      AnswerTitle: 'MockTitleb',
+      AnswerAuthor: 'MockAuthorb',
+      AnswerUrl: 'MockUrlb',
+      AnswerBlurb: 'lorem impsumb',
+      AnswerTags: 'mock tag b'
+    }
+  ]
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnswerListComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+        ],
+      declarations: [ AnswerListComponent ],
+      providers: [ AnswersService ]
     })
     .compileComponents();
   }));
@@ -16,10 +43,22 @@ describe('AnswerListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AnswerListComponent);
     component = fixture.componentInstance;
+    component.answerIds = expectedIds;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an answers array', () => {
+    expect(component.answers).toEqual([]);
+  });
+
+  it('should have an answerIds array input', () => {
+    expect(component.answerIds).toEqual(expectedIds);
+  });
+
+
 });
+
