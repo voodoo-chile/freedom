24a7e99db9c4dc8c5dabe269f8cf6b8cf0c568f6
voodoo <voodoo@blackandyellowpages.com>
2019-11-01 13:07:25 -0700

    added tests to question-show component

--- src/app/question-show/question-show.component.spec.ts
+++ src/app/question-show/question-show.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { QuestionShowComponent } from './question-show.component';
 import { AnswerShowComponent } from '../answer-show/answer-show.component';
@@ -6,10 +8,23 @@ import { AnswerShowComponent } from '../answer-show/answer-show.component';
 describe('QuestionShowComponent', () => {
   let component: QuestionShowComponent;
   let fixture: ComponentFixture<QuestionShowComponent>;
+  
+  let mockQuestion = {
+    QuestionText: 'this is a mock question',
+    QuestionAnswers: ['aa1', 'aa2'],
+    QuestionTags: ['mockTag', 'mockTag2']
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuestionShowComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+        ],
+      declarations: [ 
+        QuestionShowComponent,
+        AnswerShowComponent 
+        ]
     })
     .compileComponents();
   }));
@@ -17,10 +32,26 @@ describe('QuestionShowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionShowComponent);
     component = fixture.componentInstance;
+    component.question = mockQuestion;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a question', () => {
+    expect(component.question).not.toBe(null);
+  });
+
+  it('should show the question', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#questionText').innerHTML).toBe(mockQuestion.QuestionText);
+  });
+
+  it('should have an answer-show component', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-answer-show')).not.toBe(null);
+  });
+  
 });
