995d12a503adf3c6c2c28a59cd56ea894ec5eb09
voodoo <voodoo@blackandyellowpages.com>
2019-11-11 14:24:38 -0800

    added tests to question-get component

--- src/app/question-get/question-get.component.spec.ts
+++ src/app/question-get/question-get.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { QuestionGetComponent } from './question-get.component';
+import { AnswerListComponent } from '../answer-list/answer-list.component';
+
+import Question from '../Question';
 
 describe('QuestionGetComponent', () => {
   let component: QuestionGetComponent;
   let fixture: ComponentFixture<QuestionGetComponent>;
 
+  let mockQuestions: Question[] = [{
+    QuestionText: 'Mock question text',
+    QuestionTags: ['mockTag1', 'mockTag2'],
+    QuestionAnswers: ['aaa', 'bbb'],
+    answerTitles: ['title2', 'title2']
+  },
+  {
+    QuestionText: 'Mock question text2',
+    QuestionTags: ['mockTag1', 'mockTag2'],
+    QuestionAnswers: ['aaa', 'bbb'],
+    answerTitles: ['title1', 'title2']
+  }];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuestionGetComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ 
+        QuestionGetComponent,
+        AnswerListComponent 
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +41,29 @@ describe('QuestionGetComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionGetComponent);
     component = fixture.componentInstance;
+    component.questions = mockQuestions;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('has a questions array', () => {
+    expect(component.questions).not.toBe(null);
+    expect(component.questions).toEqual(mockQuestions);
+  });
+
+  it('has cards for each question', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('.questionCard').length).toEqual(mockQuestions.length);
+  });
+
+  it('has an answer-list component on every card', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('app-answer-list').length).toEqual(mockQuestions.length);
+    const firstCard = compiled.querySelectorAll('.questionCard')[0];
+    expect(firstCard.querySelectorAll('app-answer-list').length).toEqual(1);
+  });
+
 });
