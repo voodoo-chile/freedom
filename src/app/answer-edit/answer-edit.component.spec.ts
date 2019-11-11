9689979208bff3be537c5623dca0931132bd2433
voodoo <voodoo@blackandyellowpages.com>
2019-11-11 15:47:30 -0800

    added tests to answer-edit component

--- src/app/answer-edit/answer-edit.component.spec.ts
+++ src/app/answer-edit/answer-edit.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AnswerEditComponent } from './answer-edit.component';
 
@@ -8,6 +11,11 @@ describe('AnswerEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       declarations: [ AnswerEditComponent ]
     })
     .compileComponents();
@@ -21,5 +29,44 @@ describe('AnswerEditComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('has an answer form', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form')).toBeTruthy();
+  });
+
+  describe('answer form', () => {
+    let compiled;
+
+    beforeEach(() => {
+      compiled = fixture.debugElement.nativeElement;
+    });
+
+    it('has an AnswerTitle input', () => {
+      expect(compiled.querySelector('#answerTitleInput')).toBeTruthy();
+    });
+
+    it('has an AnswerAuthor input', () => {
+      expect(compiled.querySelector('#answerAuthorInput')).toBeTruthy();
+    });
+
+    it('has an AnswerUrl input', () => {
+      expect(compiled.querySelector('#answerUrlInput')).toBeTruthy();
+    });
+
+    it('has an AnswerBlurb input', () => {
+      expect(compiled.querySelector('#answerBlurbInput')).toBeTruthy();
+    });
+
+    it('has an AnswerTags input', () => {
+      expect(compiled.querySelector('#answerTagsInput')).toBeTruthy();
+    });
+
+    it('has a "create answer" button', () => {
+      expect(compiled.querySelector('button')).toBeTruthy();
+      expect(compiled.querySelector('button').innerHTML).toEqual('Update Answer');
+    });
+
   });
 });
