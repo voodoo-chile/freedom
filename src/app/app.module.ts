f6e1f38c66292bce1d736dae0a295ebf8431933b
voodoo <voodoo@blackandyellowpages.com>
2020-02-26 16:54:52 -0800

    removed outdated components from module list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { SlimLoadingBarModule } from "ng2-slim-loading-bar";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuestionAddComponent } from './question-add/question-add.component';
-import { QuestionGetComponent } from './question-get/question-get.component';
-import { QuestionEditComponent } from './question-edit/question-edit.component';
-import { AnswerAddComponent } from './answer-add/answer-add.component';
-import { AnswerGetComponent } from './answer-get/answer-get.component';
-import { AnswerEditComponent } from './answer-edit/answer-edit.component';
-import { CategoryAddComponent } from './category-add/category-add.component';
-import { CategoryGetComponent } from './category-get/category-get.component';
-import { CategoryEditComponent } from './category-edit/category-edit.component';
-import { TagAddComponent } from './tag-add/tag-add.component';
-import { TagGetComponent } from './tag-get/tag-get.component';
-import { TagEditComponent } from './tag-edit/tag-edit.component';
-import { TagSearchComponent } from './tag-search/tag-search.component';
-import { BlurbAddComponent } from './blurb-add/blurb-add.component';
-import { BlurbGetComponent } from './blurb-get/blurb-get.component';
-import { BlurbEditComponent } from './blurb-edit/blurb-edit.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuestionAddComponent } from "./question-add/question-add.component";
+import { QuestionGetComponent } from "./question-get/question-get.component";
+import { QuestionEditComponent } from "./question-edit/question-edit.component";
+import { AnswerAddComponent } from "./answer-add/answer-add.component";
+import { AnswerGetComponent } from "./answer-get/answer-get.component";
+import { AnswerEditComponent } from "./answer-edit/answer-edit.component";
+import { TagAddComponent } from "./tag-add/tag-add.component";
+import { TagGetComponent } from "./tag-get/tag-get.component";
+import { TagEditComponent } from "./tag-edit/tag-edit.component";
+import { TagSearchComponent } from "./tag-search/tag-search.component";
 
-import { QuestionsService } from './questions.service';
-import { QuestionShowComponent } from './question-show/question-show.component';
-import { AnswerShowComponent } from './answer-show/answer-show.component';
-import { AnswerListComponent } from './answer-list/answer-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { QuestionCarouselComponent } from './question-carousel/question-carousel.component';
-import { HomeComponent } from './home/home.component';
-import { CategoryListComponent } from './category-list/category-list.component';
+import { QuestionsService } from "./questions.service";
+import { QuestionShowComponent } from "./question-show/question-show.component";
+import { AnswerShowComponent } from "./answer-show/answer-show.component";
+import { AnswerListComponent } from "./answer-list/answer-list.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { QuestionCarouselComponent } from "./question-carousel/question-carousel.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [
@@ -43,22 +36,15 @@ import { CategoryListComponent } from './category-list/category-list.component';
     AnswerAddComponent,
     AnswerGetComponent,
     AnswerEditComponent,
-    CategoryAddComponent,
-    CategoryGetComponent,
-    CategoryEditComponent,
     TagAddComponent,
     TagGetComponent,
     TagEditComponent,
     TagSearchComponent,
-    BlurbAddComponent,
-    BlurbGetComponent,
-    BlurbEditComponent,
     QuestionShowComponent,
     AnswerShowComponent,
     AnswerListComponent,
     QuestionCarouselComponent,
-    HomeComponent,
-    CategoryListComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -69,7 +55,7 @@ import { CategoryListComponent } from './category-list/category-list.component';
     BrowserAnimationsModule,
     CarouselModule.forRoot()
   ],
-  providers: [ QuestionsService ],
+  providers: [QuestionsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
